6b1c083e560cf39b52e4b5d8b5d07383bbedefdf
Guilherme Mattos <guilhermevsmattos@outlook.com>
2021-04-20 14:02:17 -0300

    NLW dia 1 e 2

---
+++ lib/home/widgets/appbar/app_bar_widget.dart
@@ -0,0 +1,46 @@
+import 'package:DevQuiz/core/core.dart';
+import 'package:DevQuiz/home/widgets/score_card/score_card_widget.dart';
+import 'package:flutter/widgets.dart';
+
+class AppBarWidGet extends PreferredSize {
+  AppBarWidGet()
+      : super(
+          preferredSize: Size.fromHeight(250),
+          child: Container(
+            height: 250,
+            child: Stack(
+              children: [
+                Container(
+                  height: 161,
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  width: double.maxFinite,
+                  decoration: BoxDecoration(gradient: AppGradients.linear),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text.rich(TextSpan(
+                          text: "Olá, ",
+                          style: AppTextStyles.title,
+                          children: [
+                            TextSpan(
+                                text: "Guilherme Mattos",
+                                style: AppTextStyles.titleBold)
+                          ])),
+                      Container(
+                        width: 58,
+                        height: 58,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            image: DecorationImage(
+                                image: NetworkImage(
+                                    "https://avatars.githubusercontent.com/u/50272051?s=400&u=efd6255960de4f0f945ad0c98344152f833d68b3&v=4"))),
+                      )
+                    ],
+                  ),
+                ),
+                Align(alignment: Alignment(0.0, 1.0), child: ScoreCardWidget())
+              ],
+            ),
+          ),
+        );
+}
